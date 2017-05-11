56a1cfcc0636cc7c191705acbbd43c91a56d70e6
logonrm <logonrm@fiap.com.br>
2017-05-10 23:03:01 -0300

    asas

--- workspace_marceliino/Aula_08052017/src/br/com/fiap/Main.java
+++ workspace_marceliino/Aula_08052017/src/br/com/fiap/Main.java
@@ -3,14 +3,22 @@ package br.com.fiap;
 public class Main {
 	
 	public static void main(String[] asas) {
-		Aluno aluno = new Aluno();
+		/*Aluno aluno = new Aluno();
 		aluno.setNomeAluno("Marcelo");
 		aluno.setCodigoAluno(123);
 		
 		System.out.println(aluno.getNomeAluno() + " - "+ aluno.getCodigoAluno());
 		
 		Cliente cliente = new Cliente("123","456","rua asas", "Marcelo");
-		System.out.println(cliente);		
+		System.out.println(cliente);		*/
+		
+		int valor = 0;
+		
+		
+		while (int i = 0; i <= 12; i++) {
+			valor = i%2;
+			System.out.println(Integer.toString(valor));
+		}
 	}
 
 }
